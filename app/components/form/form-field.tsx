bbc8c0b7c50687bb985906b088264c61bbfe1196
Charlie W <cw5790@gmail.com>
2023-06-14 00:13:08 +0100

    Add some form components

---
+++ app/components/form/form-field.tsx
@@ -0,0 +1,65 @@
+"use client";
+import type { FocusEvent, ReactElement } from "react";
+import { forwardRef, useState } from "react";
+
+export type FormFieldErrorProps = {};
+
+export type FormFieldProps = {
+  errorMessage?: string | React.ReactElement;
+  label: string;
+  labelLeft?: boolean;
+  validateFunc?: (str: string) => boolean;
+};
+
+type Props = FormFieldProps & React.HTMLProps<HTMLInputElement>;
+
+const FormField = forwardRef<HTMLInputElement, Props>(
+  (
+    { errorMessage, label, labelLeft, name, validateFunc, ...props },
+    ref
+  ): ReactElement => {
+    const [isValidField, setIsValidField] = useState<boolean>(true);
+
+    const validateInput = (e: FocusEvent<HTMLInputElement>) => {
+      if (!validateFunc) return;
+
+      setIsValidField(validateFunc(e.currentTarget.value));
+    };
+
+    const ErrorMessage = (): ReactElement | null => {
+      if (errorMessage) {
+        if (typeof errorMessage === "string") {
+          return <p className="text-sm text-red-600">{errorMessage}</p>;
+        }
+
+        return errorMessage;
+      }
+
+      return null;
+    };
+    return (
+      <div
+        className={`mb-4 flex ${
+          labelLeft ? "flex-row items-center justify-between" : "flex-col"
+        }`}
+      >
+        <label className="block font-bold" htmlFor={name}>
+          {label}
+        </label>
+        <div className="flex flex-col">
+          <input
+            className="rounded bg-slate-200 bg-clip-padding px-3 py-2
+        text-lg autofill:first-line:text-lg focus:outline-none focus:outline-2 focus:outline-violet-400"
+            name={name}
+            {...props}
+            ref={ref}
+            onBlur={validateInput}
+          />
+          {errorMessage && <ErrorMessage />}
+        </div>
+      </div>
+    );
+  }
+);
+
+export default FormField;
---
+++ app/components/form/image-upload.tsx
@@ -0,0 +1,58 @@
+import type { ChangeEvent} from "react";
+import { useState } from "react";
+
+enum UPLOAD_PRESET_ENUM {
+  bidhubAvatar = "bidhub_avatar",
+  bidhubItem = "bidhub_item",
+}
+
+type ImageUploadProps = {
+  uploadPreset?: UPLOAD_PRESET_ENUM;
+};
+
+type Props = ImageUploadProps & React.HTMLProps<HTMLInputElement>;
+
+type ImageUploadState = {
+  fileName: string;
+  image: string;
+  largeImage: string;
+};
+
+export default function ImageUpload({
+  uploadPreset = UPLOAD_PRESET_ENUM.bidhubItem,
+  ...props
+}: Props) {
+  const [imageState, setImageState] = useState<ImageUploadState>();
+  const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+
+    if (files) {
+      const data = new FormData();
+      data.append("file", files[0]);
+      data.append("upload_preset", uploadPreset);
+
+      const res = await fetch(
+        "https://api.cloudinary.com/v1_1/bidhub/image/upload",
+        {
+          method: "POST",
+          body: data,
+        }
+      );
+
+      const file = await res.json();
+      setImageState({
+        image: file.secure_url,
+        largeImage: file.eager[0].secure_url,
+        fileName: files[0].name,
+      });
+    }
+  };
+  return (
+    <input
+      type="file"
+      onChange={uploadImage}
+      {...props}
+      value={imageState?.largeImage}
+    />
+  );
+}
