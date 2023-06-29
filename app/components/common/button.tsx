fa35b9b41f39a7ce59e4671e0e0e2f8f7f3a09b1
Charlie W <cw5790@gmail.com>
2023-06-29 23:53:59 +0100

    Add ability to use button as a link

--- app/components/common/button.tsx
+++ app/components/common/button.tsx
@@ -1,17 +1,22 @@
 import type { HTMLProps, ReactNode } from "react";
 import classNames from "classnames";
+import Link from "next/link";
 
 type ButtonProps = {
+  asLink?: boolean;
   children: ReactNode;
+  href?: string;
   type?: "button" | "submit" | "reset" | undefined; // No idea why HTMLProps<HTMLButtonElement> doesn't cover this correctly
-  variant?: "primary" | "secondary" | "danger" | "ghost";
+  variant?: "primary" | "secondary" | "neutral" | "danger" | "ghost";
 };
 type Props = ButtonProps & HTMLProps<HTMLButtonElement>;
 
 export default function Button({
+  asLink = false,
   children,
   className,
-  type,
+  href,
+  type = "button",
   variant = "primary",
   ...props
 }: Props) {
@@ -20,11 +25,21 @@ export default function Button({
     {
       "button-primary": variant === "primary",
       "button-secondary": variant === "secondary",
+      "button-neutral": variant === "neutral",
       "button-danger": variant === "danger",
       "button-ghost": variant === "ghost",
     },
     className
   );
+
+  if (asLink && href) {
+    return (
+      <Link className={variantClass} href={href}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <button className={variantClass} type={type} {...props}>
       {children}
