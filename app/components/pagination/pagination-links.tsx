a149e2f8ffeb76d77dfccd78aa2d90dd333b917d
Charlie W <cw5790@gmail.com>
2023-07-02 19:25:52 +0100

    Update pagination component but still needs work

--- app/components/pagination/pagination-links.tsx
+++ app/components/pagination/pagination-links.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { usePathname, useSearchParams } from "next/navigation";
 import Button from "../common/button";
+import { useMemo } from "react";
 
 type Props = {
   currentPage: number;
+  maxPagesShown?: number;
   numberOfPages: number;
 };
 
@@ -11,13 +13,57 @@ type Props = {
  * Going to take this component back to the old school a bit and control it through the URL.
  * It feels logical for it to behave that way
  */
-export default function PaginationLinks({ numberOfPages }: Props) {
+export default function PaginationLinks({
+  maxPagesShown = 5,
+  numberOfPages,
+}: Props) {
   const pathName = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = searchParams.get("page") || 1;
+  const currentPage = searchParams.get("page")
+    ? Number(searchParams.get("page"))
+    : 1;
+
+  const getPagesToDisplay = () => {
+    const isCurrentPageFirstPage = currentPage === 1;
+    const isCurrentPageLastPage = currentPage === numberOfPages;
+    const doesNumberOfPagesExceedMaxNumberOfPages =
+      numberOfPages > maxPagesShown;
+
+    if (!doesNumberOfPagesExceedMaxNumberOfPages) {
+      return Array.from({ length: numberOfPages }, (_, i) => i + 1);
+    }
+
+    if (isCurrentPageFirstPage) {
+      return Array.from({ length: maxPagesShown }, (_, i) => i + 1).concat([
+        numberOfPages,
+      ]);
+    }
+
+    if (isCurrentPageLastPage) {
+      const lastPageMinusFive = currentPage - 4;
+      if (lastPageMinusFive < 1) {
+        return Array.from({ length: maxPagesShown }, (_, i) => i + 1);
+      }
+
+      return [1].concat(
+        Array.from({ length: maxPagesShown }, (_, i) => lastPageMinusFive + i)
+      );
+    }
+
+    /**
+     * TODO:
+     Logic for a mid page like page 4 of 8
+     We want to display pages 1... 3 4 5 ... 8 maybe?
+     Will revisit the logic
+     * 
+     */
+
+    return Array.from({ length: numberOfPages }, (_, i) => i + 1);
+  };
+
   if (numberOfPages <= 1) return null;
 
-  let pages = Array.from({ length: numberOfPages }, (_, i) => i + 1);
+  const pages = useMemo(() => getPagesToDisplay(), [currentPage]);
   return (
     <div className="flex gap-1">
       {pages.map((page) => {
